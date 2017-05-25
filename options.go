9c0f1d1a18812d00345f3e4e54806800aa04af67
Veeru Natarajan <veeruns@yahoo.com>
2017-05-25 11:42:20 -0700

    option to options

--- options.go
+++ options.go
@@ -5,12 +5,13 @@ import (
 "fmt"
 "flag"
 )
-type Options struct {
+type Option struct {
   Logfile string
   Loglevel string
   Concurrency int
   Timeout int
 }
+var Options Option
 /*
 var Logfile string
 var Loglevel string
@@ -19,13 +20,14 @@ var Timeout int
 */
 func Flagoperation(){
 
-  flag.StringVar(Options.Logfile,"logfile","default.log","Log filename to write")
 
-  flag.StringVar(Options.Loglevel,"loglevel","INFO","Log level")
+  flag.StringVar(&Options.Logfile,"logfile","default.log","Log filename to write")
 
-  flag.IntVar(Options.Concurrency,"concurrency",2,"Number of threads")
+  flag.StringVar(&Options.Loglevel,"loglevel","INFO","Log level")
 
-  flag.IntVar(Options.Timeout,"timeout",2000,"Default timeout for any operations")
+  flag.IntVar(&Options.Concurrency,"concurrency",2,"Number of threads")
+
+  flag.IntVar(&Options.Timeout,"timeout",2000,"Default timeout for any operations")
   fmt.Printf("Done\n")
   flag.Parse()
 }
